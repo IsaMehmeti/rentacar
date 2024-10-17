106157e7efd3284fc87104b929353585741ddc3b
Isa Mehmeti <isa-mehmeti@hotmail.com>
2024-10-17 13:25:08 +0200

    Authentication expiry, Theme Change

--- resources/js/hooks/useAuth.js
+++
@@ -1,7 +0,0 @@
-import { useMutation, useQuery } from "@tanstack/vue-query";
-
-// Initialize the auth store
-const authStore = useAuthStore();
-export function useAuth(credentials) {
-    return {};
-}
